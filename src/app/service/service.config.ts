6e4493d7809cd3cbdefe4b824a1e337dbc00f577
renyuzhe <ren_yuzhe@126.com>
2017-06-20 23:39:09 +0800

    finished

--- src/app/service/service.config.ts
+++ src/app/service/service.config.ts
@@ -1,5 +1,5 @@
-export const serviceAdd:string = "192.168.43.193";
-// export const serviceAdd: string = "localhost";
+// export const serviceAdd:string = "192.168.43.193";
+export const serviceAdd: string = "localhost";
 export const servicePort: string = "8080";
 export const service: string = 'http://' + serviceAdd + ':' + servicePort;
 
